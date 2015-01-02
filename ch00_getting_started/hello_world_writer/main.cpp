6af94a74d08cbf373c1a3ced66f31a70e9a13eaf
chrzhang <christopherzhang3@gmail.com>
2015-01-02 20:04:54 +0000

    Finished Chapter 0 exercises.

---
+++ ch00_getting_started/hello_world_writer/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+
+    std::cout << "#include <iostream>\n"
+              << "int main() {\n\tstd::cout <<"
+              << " \"Hello, world!\" << std::endl;\n"
+              << "\treturn 0;\n}" << std::endl;
+    return 0;
+
+}
---
+++ ch00_getting_started/tabs/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+
+    std::cout << "Here's how \\t tabs are shown (delimited by _): _\t_"
+              << std::endl;
+    return 0;
+
+}
