101358739d081d394ff17984f2d88c6bb9d4b0e7
chrzhang <christopherzhang3@gmail.com>
2015-01-10 15:34:08 +0000

    Some of Chapter 5, mainly permuted index and list versus vector wars.

---
+++ ch05_using_sequential_containers_and_analyzing_strings/list_vs_vec/Student_info.cpp
@@ -0,0 +1,142 @@
+#include "Student_info.h"
+#include "grade.h"
+#include <iostream>
+#include <list>
+#include <vector>
+
+#define FAIL_CUTOFF 60
+
+using std::istream;
+using std::vector;
+using std::cout;
+using std::endl;
+using std::list;
+
+bool compare(const Student_info & x, const Student_info & y) {
+
+    return x.name < y.name;
+
+}
+
+istream & read(istream & is, Student_info & s) {
+
+    is >> s.name >> s.midterm >> s.final;
+
+    read_hw(is, s.homework);
+
+    return is;
+
+}
+
+istream & read_hw(istream & in, vector<double> & hw) {
+
+    if (in) {
+        hw.clear();
+
+        double x;
+        while (in >> x)
+            hw.push_back(x);
+
+        in.clear();
+    }
+
+    return in;
+
+}
+
+bool fgrade(const Student_info & s) {
+
+    return grade(s) < FAIL_CUTOFF;
+
+}
+
+vector<Student_info> extract_fails1(vector<Student_info> & students) {
+
+    // Needs twice the space as the original vector of student to store
+    // intermediate pass and fail vectors
+    vector<Student_info> pass, fail;
+
+    for (vector<Student_info>::size_type i = 0; i != students.size(); i++) {
+
+        if (fgrade(students[i]))
+            fail.push_back(students[i]);
+        else
+            pass.push_back(students[i]);
+    }
+
+    students = pass;
+
+    return fail;
+
+}
+
+vector<Student_info> extract_fails2(vector<Student_info> & students) {
+
+    // Remove any failing students from the vector as they are encountered
+    vector<Student_info> fail;
+    vector<Student_info>::size_type i = 0;
+
+    while (i != students.size()) {
+
+        if (fgrade(students[i])) {
+            fail.push_back(students[i]);
+            students.erase(students.begin() + i);
+        } else
+            i++;
+
+    }
+
+    return fail;
+
+}
+
+vector<Student_info> extract_fails3(vector<Student_info> & students) {
+
+    // Use read-only iterators instead of indexing
+    vector<Student_info> fail;
+    vector<Student_info>::iterator iter = students.begin();
+    while (iter != students.end()) {
+
+        if (fgrade(*iter)) {
+            fail.push_back(*iter);
+            // erase() returns the following element
+            iter = students.erase(iter);
+            // erase() invalidates all iterators after point erased
+        } else
+            ++iter;
+
+    }
+
+    return fail;
+
+}
+
+list<Student_info> extract_fails4(list<Student_info> & students) {
+
+    list<Student_info> fail;
+    list<Student_info>::iterator iter = students.begin();
+
+    while (iter != students.end()) {
+
+        if (fgrade(*iter)) {
+            fail.push_back(*iter);
+            iter = students.erase(iter);
+        } else
+            iter++;
+
+    }
+
+    return fail;
+
+}
+
+void display(const vector<Student_info> & students) {
+
+    // const_iterator specifies a read-only iterator
+    for (vector<Student_info>::const_iterator iter = students.begin();
+         iter != students.end(); iter++) {
+
+        cout << iter-> name << endl;
+
+    }
+}
---
+++ ch05_using_sequential_containers_and_analyzing_strings/list_vs_vec/main.cpp
@@ -0,0 +1,104 @@
+#include <iostream>
+#include <random>
+#include <stdexcept> // For domain_error
+#include <vector>
+#include <list>
+#include <algorithm> // For sort()
+#include <string>
+#include <iomanip> // For setprecision()
+#include <ios> // For streamsize
+#include <istream>
+#include <fstream>
+#include "grade.h"
+#include <iterator> // For begin() and end()
+#include "Student_info.h"
+#include <ctime> // To measure performance
+
+using std::vector;
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+using std::domain_error;
+using std::istream;
+using std::streamsize;
+using std::setprecision;
+using std::sort;
+using std::max;
+using std::ifstream;
+using std::list;
+using std::begin;
+using std::end;
+using std::rand;
+using std::default_random_engine;
+using std::uniform_int_distribution;
+using std::ofstream;
+using std::clock;
+
+/*
+Calculate grades for students where grades are entered with
+[name] [midterm_score] [final_exam_score] [hw1 score] [hw2 score] ...
+followed by any additional students.
+*/
+int main() {
+
+    cout << "Creating sample file." << endl;
+    ofstream f;
+    f.open("input.txt");
+
+    cout << "Enter sample size: ";
+    int n = 0;
+    cin >> n;
+
+    default_random_engine generator;
+    uniform_int_distribution<int> distribution(1, 100);
+
+    for (int i = 0; i < n; i++) {
+
+        f << "Foo";
+
+        for (int j = 0; j < 5; j++) // Generate scores
+            f << " " << distribution(generator);
+
+        f << endl;
+
+    }
+
+    f.close();
+
+    vector<Student_info> students;
+    Student_info record;
+    string::size_type maxlen = 0;
+
+    ifstream myfile("input.txt");
+    if (!myfile.is_open()) {
+        cout << "Unable to open file" << endl;
+        return -1;
+    }
+    while (read(myfile, record)) {
+
+        maxlen = max(maxlen, record.name.size());
+        students.push_back(record);
+
+    }
+    myfile.close();
+
+    sort(students.begin(), students.end(), compare);
+
+    list<Student_info> studentsList{ begin(students), end(students) };
+    cout << "Extracting fails with vectors." << endl;
+    clock_t begin = clock();
+    extract_fails3(students); // With vectors
+    clock_t end = clock();
+    double elapsed = double(end - begin) / CLOCKS_PER_SEC;
+    cout << "Done. Elapsed " << elapsed << endl;
+    cout << "Extracting fails with lists." << endl;
+    begin = clock();
+    extract_fails4(studentsList); // With lists
+    end = clock();
+    elapsed = double(end - begin) / CLOCKS_PER_SEC;
+    cout << "Done. Elapsed " << elapsed << endl;
+
+    return 0;
+
+}
