5e3a0b1f56c066511e7cdf1ed69b9106c5187578
chrzhang <christopherzhang3@gmail.com>
2015-01-10 22:59:30 +0000

    Finished Chapter 5.

--- ch05_using_sequential_containers_and_analyzing_strings/td/median.h
+++
@@ -1,9 +0,0 @@
-#ifndef GUARD_median_h
-#define GUARD_median_h
-
-#include <vector>
-
-// Use fully-qualified names in headers and avoid 'using' to improve flexibility
-double median(std::vector<double>);
-
-#endif
